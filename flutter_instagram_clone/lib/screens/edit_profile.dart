3fcd87eb3bec80bd296e76f6ff727aea623c33c6
jubaricode <43369339+jubaricode@users.noreply.github.com>
2019-12-05 17:15:03 +0700

    User Profile (Part 2) | Flutter Firebase Instagram Tutorial 2019 (Ep 5) Done

--- flutter_instagram_clone/lib/screens/edit_profile.dart
+++ flutter_instagram_clone/lib/screens/edit_profile.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_instagram_clone/models/user_model.dart';
 import 'package:flutter_instagram_clone/services/database_service.dart';
+import 'package:flutter_instagram_clone/services/storage_service.dart';
+import 'package:image_picker/image_picker.dart';
 
 class EditProfileScreen extends StatefulWidget {
   final User user;
@@ -12,8 +17,10 @@ class EditProfileScreen extends StatefulWidget {
 
 class _EditProfileScreenState extends State<EditProfileScreen> {
   final _formKey = GlobalKey<FormState>();
+  File _profileImage;
   String _name = '';
   String _bio = '';
+  bool _isLoading = false;
 
   initState() {
     super.initState();
@@ -21,11 +28,52 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
     _bio = widget.user.bio;
   }
 
-  _submit() {
+  _handleImageFromGallery() async {
+    File imageFile = await ImagePicker.pickImage(source: ImageSource.gallery);
+    if (imageFile != null) {
+      setState(() {
+        _profileImage = imageFile;
+      });
+    }
+  }
+
+  _displayProfileImage() {
+    // No new profile image
+    if (_profileImage == null) {
+      // No existing profile image
+      if (widget.user.profileImageUrl.isEmpty) {
+        //Display PlaceHolder
+        return AssetImage('assets/images/user_placeholder.jpg');
+      } else {
+        // User Profile image exists
+        return CachedNetworkImageProvider(widget.user.profileImageUrl);
+      }
+    } else {
+      // New Profile image
+      return FileImage(_profileImage);
+    }
+  }
+
+  _submit() async {
     if (_formKey.currentState.validate()) {
       _formKey.currentState.save();
+
+      setState(() {
+        _isLoading = true;
+      });
+
       // Update user in database
       String _profileImageUrl = '';
+
+      if (_profileImage == null) {
+        _profileImageUrl = widget.user.profileImageUrl;
+      } else {
+        _profileImageUrl = await StorageServices.uploadUserProfileImage(
+          widget.user.profileImageUrl,
+          _profileImage,
+        );
+      }
+
       User user = User(
         id: widget.user.id,
         name: _name,
@@ -47,75 +95,83 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
         backgroundColor: Colors.white,
         title: Text('Edit Profile'),
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: EdgeInsets.all(30.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  CircleAvatar(
-                    radius: 60.0,
-                    backgroundImage: NetworkImage(
-                        'https://upload.wikimedia.org/wikipedia/commons/2/2f/Wylde.cropped.png'),
-                  ),
-                  FlatButton(
-                    onPressed: () => print("Change Profile Image"),
-                    child: Text('Change Profile Image',
-                        style: TextStyle(
-                            color: Theme.of(context).accentColor,
-                            fontSize: 16.0)),
-                  ),
-                  TextFormField(
-                    initialValue: _name,
-                    style: TextStyle(fontSize: 18.0),
-                    decoration: InputDecoration(
-                      icon: Icon(
-                        Icons.person,
-                        size: 30.0,
-                      ),
-                      labelText: 'Name',
-                    ),
-                    validator: (input) => input.trim().length < 1
-                        ? 'Please enter a valid name'
-                        : null,
-                    onSaved: (input) => _name = input,
-                  ),
-                  TextFormField(
-                    initialValue: _bio,
-                    style: TextStyle(fontSize: 18.0),
-                    decoration: InputDecoration(
-                      icon: Icon(
-                        Icons.book,
-                        size: 30.0,
-                      ),
-                      labelText: 'Bio',
-                    ),
-                    validator: (input) => input.trim().length > 150
-                        ? 'Please enter a bio less than 150 character'
-                        : null,
-                    onSaved: (input) => _bio = input,
-                  ),
-                  Container(
-                    margin: EdgeInsets.all(40.0),
-                    height: 40.0,
-                    width: 250.0,
-                    child: FlatButton(
-                      onPressed: _submit,
-                      color: Colors.blue,
-                      textColor: Colors.white,
-                      child: Text(
-                        'Save Profile',
-                        style: TextStyle(fontSize: 18.0),
-                      ),
-                    ),
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: ListView(
+          children: <Widget>[
+            _isLoading
+                ? LinearProgressIndicator(
+                    backgroundColor: Colors.blue[200],
+                    valueColor: AlwaysStoppedAnimation(Colors.blue),
                   )
-                ],
+                : SizedBox.shrink(),
+            Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: 60.0,
+                      backgroundColor: Colors.grey,
+                      backgroundImage: _displayProfileImage(),
+                    ),
+                    FlatButton(
+                      onPressed: _handleImageFromGallery,
+                      child: Text('Change Profile Image',
+                          style: TextStyle(
+                              color: Theme.of(context).accentColor,
+                              fontSize: 16.0)),
+                    ),
+                    TextFormField(
+                      initialValue: _name,
+                      style: TextStyle(fontSize: 18.0),
+                      decoration: InputDecoration(
+                        icon: Icon(
+                          Icons.person,
+                          size: 30.0,
+                        ),
+                        labelText: 'Name',
+                      ),
+                      validator: (input) => input.trim().length < 1
+                          ? 'Please enter a valid name'
+                          : null,
+                      onSaved: (input) => _name = input,
+                    ),
+                    TextFormField(
+                      initialValue: _bio,
+                      style: TextStyle(fontSize: 18.0),
+                      decoration: InputDecoration(
+                        icon: Icon(
+                          Icons.book,
+                          size: 30.0,
+                        ),
+                        labelText: 'Bio',
+                      ),
+                      validator: (input) => input.trim().length > 150
+                          ? 'Please enter a bio less than 150 character'
+                          : null,
+                      onSaved: (input) => _bio = input,
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(40.0),
+                      height: 40.0,
+                      width: 250.0,
+                      child: FlatButton(
+                        onPressed: _submit,
+                        color: Colors.blue,
+                        textColor: Colors.white,
+                        child: Text(
+                          'Save Profile',
+                          style: TextStyle(fontSize: 18.0),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
--- flutter_instagram_clone/lib/screens/profile_screen.dart
+++ flutter_instagram_clone/lib/screens/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_instagram_clone/models/user_model.dart';
 import 'package:flutter_instagram_clone/screens/edit_profile.dart';
@@ -37,8 +38,10 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     children: <Widget>[
                       CircleAvatar(
                         radius: 50.0,
-                        backgroundImage: NetworkImage(
-                            'https://upload.wikimedia.org/wikipedia/commons/2/2f/Wylde.cropped.png'),
+                        backgroundColor: Colors.grey,
+                        backgroundImage: user.profileImageUrl.isEmpty
+                            ? AssetImage('assets/images/user_placeholder.jpg')
+                            : CachedNetworkImageProvider(user.profileImageUrl),
                       ),
                       Expanded(
                         child: Column(
